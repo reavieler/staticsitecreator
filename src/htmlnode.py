1b20680f7b9ff682b49e5ac37f632af4900b1237
reavieler <miguel.vgloria@gmail.com>
2024-04-11 20:47:26 +0100

    Added ParentNode class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -38,5 +38,26 @@ class LeafNode(HTMLNode):
             props_value = self.props_to_html()
             return f"<{self.tag}{props_value}>{self.value}</{self.tag}>"
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag = None,value = None, children = None, props = None):
+        if value is not None:
+            raise ValueError('No Parent nodes can have a value!')
+        if children is None or children == []:
+            raise ValueError('Parent nodes must have children!')
+        if tag is None:
+            raise ValueError('Need to provide tag to parent node!')
+        super().__init__(tag, value, children, props)
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+    def to_html(self):
+        output = ""
+        for child in self.children:
+            output += child.to_html()
+        return f"<{self.tag}>{output}</{self.tag}>"
+
+        
+
 
 
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -8,15 +8,30 @@ class TestHTMLNode(unittest.TestCase):
         leaf1 = LeafNode("p", "This is a paragraph of text.")
         leaf2 = LeafNode("a", "Click me!",None,{"href": "https://www.google.com"})
 
-        print(leaf1.__repr__())
-        print(leaf2.__repr__())
-        self.assertEqual(leaf1.tag, 'p')
-        self.assertEqual(leaf1.value, 'This is a paragraph of text.')
-        self.assertIsNone(leaf1.children, "LeafNodes should not have children classes associated.")
-        self.assertEqual(leaf1.to_html(), "<p>This is a paragraph of text.</p>")
+        # print(leaf1.__repr__())
+        # print(leaf2.__repr__())
+        # self.assertEqual(leaf1.tag, 'p')
+        # self.assertEqual(leaf1.value, 'This is a paragraph of text.')
+        # self.assertIsNone(leaf1.children, "LeafNodes should not have children classes associated.")
+        # self.assertEqual(leaf1.to_html(), "<p>This is a paragraph of text.</p>")
 
 
-        print(leaf2.to_html())
+        # print(leaf2.to_html())
+
+
+        node = ParentNode(
+            "p",None,
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+
+        print(node.__repr__())
+
+        print(node.to_html())
 
 
 if __name__ == "__main__":
