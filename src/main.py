d8ee3768cf2311819f91fd82bab8f8afa77b9d81
reavieler <miguel.vgloria@gmail.com>
2024-04-12 11:57:11 +0100

    added split_nodes_delimiter. It creates new text nodes based on textnodes that have markdown type language

--- src/main.py
+++ src/main.py
@@ -2,7 +2,12 @@ from textnode import *
 from htmlnode import *
 
 text_types = ["text", "bold", "italic", "code", "link", "image"]
-
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
 
 
 
@@ -23,11 +28,32 @@ def text_node_to_html_node(text_node):
         return LeafNode("img", "", None, {"src": text_node.url, "alt": text_node.text})
 
 
+def split_nodes_delimiter(old_nodes, delimiter, texttype):
+    nodesoutput = []
+    for node in old_nodes:
+        if node.text_type != text_type_text:
+            nodesoutput.append(node)
+        else:
+            if node.text.count(delimiter) % 2 != 0 or node.text.count(delimiter) == 0:
+                raise ValueError("Invalid Markdown syntax! Every delimiter should be accompanied by a closing delimiter.")
+            node_split_by_delimiter = node.text.split(delimiter)
+            print(node_split_by_delimiter)
+            for i in range(len(node_split_by_delimiter)):
+                if i % 2 == 0:
+                    nodesoutput.append(TextNode(node_split_by_delimiter[i], text_type_text))
+                else:
+                    nodesoutput.append(TextNode(node_split_by_delimiter[i], texttype))
 
+    print(nodesoutput)
+    return nodesoutput
 
 def main():
     
-    new_text_node = TextNode("texto de teste!!","text")
-    text_node_to_html_node(new_text_node)
+    node = TextNode("This is text with a **bold!!!** word", text_type_text)
+    new_nodes = split_nodes_delimiter([node],"**",text_type_bold)
+    print(new_nodes)
 
 main()
+
+
+#git commit -m "added split_nodes_delimiter. It creates new text nodes based on textnodes that have markdown type language"
