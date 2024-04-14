5459d72f060545e7b63bddd95ad6fbf1a66965de
reavieler <miguel.vgloria@gmail.com>
2024-04-14 22:51:34 +0100

    finished apply changes to textnodes and started working on splitting blocks into lists and identifying blocks

--- src/main.py
+++ src/main.py
@@ -2,21 +2,23 @@ from textnode import *
 from htmlnode import *
 import re
 
+#define possible text types
 text_types = ["text", "bold", "italic", "code", "link", "image"]
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
-# possible_type_dict = {
-#     "text":"",
-#     "bold": "**",
-#     "italic": "*",
-#     "code": "`"
-# }
+text_type_text, text_type_bold, text_type_italic, text_type_code, text_type_link, text_type_image = "text", "bold", "italic", "code", "link", "image"
 
+#define possible block types
+block_type_paragraph, block_type_heading, block_type_code = "paragraph", "heading", "code"
+block_type_quote, block_type_unordered_list, block_type_ordered_list = "quote", "unordered_list", "ordered_list"
 
+def escape_special_characters(text):
+    # Replace '\*' and '\`' with a placeholder
+    text = text.replace("\\*", "ESCAPED_ASTERISK").replace("\\`", "ESCAPED_BACKTICK")
+    return text
+
+def unescape_special_characters(text):
+    # Replace the placeholder to literal '*' in final output
+    text = text.replace("ESCAPED_ASTERISK", "*").replace("ESCAPED_BACKTICK", "`")
+    return text
 
 def text_node_to_html_node(text_node):
     if text_node.text_type not in text_types:
@@ -46,9 +48,9 @@ def split_nodes_delimiter(old_nodes, delimiter, texttype):
             node_split_by_delimiter = node.text.split(delimiter)
             for i in range(len(node_split_by_delimiter)):
                 if i % 2 == 0:
-                    nodesoutput.append(TextNode(node_split_by_delimiter[i], text_type_text))
+                    nodesoutput.append(TextNode(unescape_special_characters(node_split_by_delimiter[i]), text_type_text))
                 else:
-                    nodesoutput.append(TextNode(node_split_by_delimiter[i], texttype))
+                    nodesoutput.append(TextNode(unescape_special_characters(node_split_by_delimiter[i]), texttype))
 
     return nodesoutput
 
@@ -63,17 +65,14 @@ def split_nodes_image(old_nodes):
     for node in old_nodes:
         original_text = node.text
         image_list = extract_markdown_images(node.text)
-        # if not image_list:
-        #     print(node)
-        #     nodesoutput.append(node)
         for image_tup in image_list:
             modified_text = original_text.split(f"![{image_tup[0]}]({image_tup[1]})")
             first_node = modified_text[0]
 
-            nodesoutput.append(TextNode(first_node,text_type_text))
-            nodesoutput.append(TextNode(image_tup[0],text_type_image,image_tup[1]))
+            nodesoutput.append(TextNode(unescape_special_characters(first_node),text_type_text))
+            nodesoutput.append(TextNode(unescape_special_characters(image_tup[0]),text_type_image,image_tup[1]))
             original_text = modified_text[1]
-        nodesoutput.append(TextNode(original_text,text_type_text))
+        nodesoutput.append(TextNode(unescape_special_characters(original_text),text_type_text))
     return nodesoutput
 
 def split_nodes_link(old_nodes):
@@ -81,29 +80,25 @@ def split_nodes_link(old_nodes):
     for node in old_nodes:
         original_text = node.text
         link_list = extract_markdown_links(node.text)
-        # if not link_list:
-        #     nodesoutput.append(node)
         for link_tup in link_list:
             modified_text = original_text.split(f"[{link_tup[0]}]({link_tup[1]})")
             first_node = modified_text[0]
-            nodesoutput.append(TextNode(first_node,text_type_text))
-            nodesoutput.append(TextNode(link_tup[0],text_type_image,link_tup[1]))
+            nodesoutput.append(TextNode(unescape_special_characters(first_node),text_type_text))
+            nodesoutput.append(TextNode(unescape_special_characters(link_tup[0]),text_type_image,link_tup[1]))
             original_text = modified_text[1]
-        nodesoutput.append(TextNode(original_text,text_type_text))
+        nodesoutput.append(TextNode(unescape_special_characters(original_text),text_type_text))
     return nodesoutput
 
 
-def apply_changes_to_textnodes(master_node_list, function_to_call, delimiter = None, text_type = None):
+def apply_changes_to_textnodes(master_node_list, processing_function, delimiter = None, text_type = None):
     changes = []
     #Step 2: Iterate on master list and collect changes in new list
     for i, node in enumerate(master_node_list):
         if node.text_type != text_type:
-            if function_to_call == 'split_nodes_delimiter':
-                split_list = split_nodes_delimiter([node], delimiter, text_type)
-            elif function_to_call == 'split_nodes_image':
-                split_list = split_nodes_image([node])
-            elif function_to_call == 'split_nodes_link':
-                split_list = split_nodes_link([node])
+            if delimiter is not None:
+                split_list = processing_function([node], delimiter, text_type)
+            else:
+                split_list = processing_function([node])
             if len(split_list) != 1:
                 changes.append((i,split_list))
     
@@ -121,19 +116,39 @@ def text_to_textnodes(text):
     master_node_list = split_nodes_delimiter(master_node_list, '**', text_type_bold)
 
     #Apply italic treatment    
-    master_node_list = apply_changes_to_textnodes(master_node_list, 'split_nodes_delimiter', '*', text_type_italic)
+    master_node_list = apply_changes_to_textnodes(master_node_list, split_nodes_delimiter, '*', text_type_italic)
 
     #Apply code treatment
-    master_node_list = apply_changes_to_textnodes(master_node_list, 'split_nodes_delimiter', '`', text_type_code)
+    master_node_list = apply_changes_to_textnodes(master_node_list, split_nodes_delimiter, '`', text_type_code)
 
     #Aply image treatment
-    master_node_list = apply_changes_to_textnodes(master_node_list, 'split_nodes_image')
+    master_node_list = apply_changes_to_textnodes(master_node_list, split_nodes_image)
 
     #Aply link treatment
-    master_node_list = apply_changes_to_textnodes(master_node_list, 'split_nodes_link')
+    master_node_list = apply_changes_to_textnodes(master_node_list, split_nodes_link)
 
     return master_node_list
 
+
+def markdown_to_blocks(markdown):
+    markdown_list = [block for block in markdown.split('\n\n')]
+    for i,value in enumerate(markdown_list):
+        markdown_list[i] = value.strip()
+    return markdown_list
+
+def block_to_block_type(block_text):
+    if re.findall(r"^#{1,6} ",block_text):
+        return block_type_heading
+    if re.findall(r"^```.*\n*```$",block_text):
+        return block_type_code
+    if len(re.findall(r"^> ",block_text, re.MULTILINE)) == block_text.count('\n') + 1:
+        return block_type_quote
+    if len(re.findall(r"^[*-] ",block_text, re.MULTILINE)) == block_text.count('\n') + 1:
+        return block_type_unordered_list
+    #TODO: NEED TO CHECK FOR ORDERED LIST PATTERN
+
+    return block_type_paragraph 
+    
 
 def main():
     
@@ -159,10 +174,34 @@ def main():
     # print(new_nodes)
 
 
-    text_to_parse = "This is **text** with an *italic* word and a `code block` and an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and a [link](https://boot.dev)"
-    print(text_to_parse)
-    test = text_to_textnodes(text_to_parse)
-    print(test)
+    # text_to_parse = "This is **text** with an *italic* word and a `code block` **and a rude word ** ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png)[link](https://boot.dev)"
+    # treated_text_to_parse =escape_special_characters(text_to_parse)
+    # print(text_to_parse)
+    # print('\n')
+    # print(treated_text_to_parse)
+    # test = text_to_textnodes(treated_text_to_parse)
+    # print(test)
+
+#     markdown_document ="""     This is **bolded** paragraph     
+
+# This is another paragraph with *italic* text and `code` here
+# This is the same paragraph on a new line
+
+
+     
+# * This is a list      
+# * with items
+# """
+#     print(markdown_document)
+#     print(markdown_to_blocks(markdown_document))
+
+
+    block_text = """> This is an unordered list!
+> This is a continuation!
+> This is another continuation!
+> blabalba"""
+    print(block_to_block_type(block_text))
+    
 
 
     
